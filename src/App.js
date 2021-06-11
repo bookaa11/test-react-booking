8ac837aea0841ed048d7298cd129ae281de48402
Abdur Rahman <onlinepurpose@yandex.com>
2021-06-11 10:37:00 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import { React, Component } from 'react';
+
+class App extends Component {
+
+  render () {
+    return (
+      <div className="container">
+        <div className="row">
+        props
+        </div>
+  
+      </div>
+  
+    );
+  }
+    
+  
+}
+
+export default App;
